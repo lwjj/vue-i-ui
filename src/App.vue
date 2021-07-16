ca80712ad8696f0a1ae5be2b60b9e05f2c9f8b47
liweijia <liweijia@qiyi.com>
2021-07-16 18:16:38 +0800

    init-commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <div class="row">
+      <i-button>按钮</i-button>
+      <i-button @click="fn" type="primary">按钮</i-button>
+      <i-button type="success">按钮</i-button>
+      <i-button type="warning">按钮</i-button>
+      <i-button type="info">按钮</i-button>
+      <i-button type="danger">按钮</i-button>
+    </div>
+    <div class="row">
+      <i-button plain round>按钮</i-button>
+      <i-button icon="one-icon-child" plain round type="primary">按钮</i-button>
+      <i-button icon="one-icon-child" plain round type="success">按钮</i-button>
+      <i-button plain round type="warning">按钮</i-button>
+      <i-button plain round type="info">按钮</i-button>
+      <i-button plain round type="danger">按钮</i-button>
+    </div>
+    <div class="row">
+      <i-button plain circle>拆</i-button>
+      <i-button plain circle type="primary">拆</i-button>
+      <i-button plain circle type="success">拆</i-button>
+      <i-button plain circle type="warning">拆</i-button>
+      <i-button plain circle type="info">拆</i-button>
+      <i-button plain circle type="danger">拆</i-button>
+    </div>
+    <div class="row">
+      <i-button plain circle icon="one-icon-child"></i-button>
+      <i-button plain circle icon="one-icon-child" type="primary"></i-button>
+      <i-button plain circle icon="one-icon-child" type="success"></i-button>
+      <i-button plain circle icon="one-icon-child" type="warning"></i-button>
+      <i-button plain circle icon="one-icon-child" type="info"></i-button>
+      <i-button plain circle disabled icon="one-icon-child" type="danger"></i-button>
+    </div> -->
+    <i-button type="primary" @click="open">打开</i-button>
+    <i-dialog title="温馨提示" width="30%" :visible.sync="visible">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+      <template #footer>
+        <i-button type="primary" @click="visible = false">确定</i-button>
+        <i-button type="primary" @click="visible = false" plain>取消</i-button>
+      </template>
+    </i-dialog>
+    <!-- <i-dialog>
+      <template #title><h2>这是标题</h2></template>
+    </i-dialog> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    fn () {
+      console.log(123)
+    },
+    open () {
+      this.visible = !this.visible
+    },
+    close (val) {
+      this.visible = val
+    }
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.row {
+  margin-bottom: 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import IButton from './components/button.vue'
+import './assets/fonts/iconfont.css'
+import IDialog from './components/dialog.vue'
+
+Vue.component(IButton.name, IButton)
+Vue.component(IDialog.name, IDialog)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
